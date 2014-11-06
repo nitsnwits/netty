1b12def279fe036490fe7008ae6d20d22c01a95f
Dhananjay Apte <dhananjayapte@gmail.com>
2014-11-05 23:20:36 -0800

    equal ring area

--- src/poke/consistentHash/EqualAreaRing.java
+++ src/poke/consistentHash/EqualAreaRing.java
@@ -1,8 +1,18 @@
 package poke.consistentHash;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.TreeMap;
+
 import poke.hash.Ketama;
+import poke.server.conf.HashRangeMap;
 
 public class EqualAreaRing extends DataRing {
+	private TreeMap<Long, List<DataNode>> testMap = HashRangeMap.getInstance().getRangeMap();
+	private List<Long> keyList = new ArrayList<Long>();
+	
 	public EqualAreaRing() {
 	}
 
@@ -12,6 +22,9 @@ public class EqualAreaRing extends DataRing {
 		long maxN = Long.MAX_VALUE;
 		long range = maxN / numNodes;
 
+		
+		
+		
 		for (int i = 0; i < numNodes; i++) {
 			DataNode dn = new DataNode();
 			dn.setId("node-" + i);
@@ -19,12 +32,55 @@ public class EqualAreaRing extends DataRing {
 			Ketama k = new Ketama();
 			dn.setHash(k);
 
-			if (i != 0)
+			long keyRange = -1;
+			
+			if (i != 0){
+				keyRange = range * (i + 1);
 				dn.setHashLimit(range * (i + 1));
-			else
+			}
+			else{
+				keyRange = maxN;
 				dn.setHashLimit(maxN);
-
+			}
+			
+			//add range and corresponding node to the list
+			List<DataNode> dataNodeList = new ArrayList<DataNode>();
+			dataNodeList.add(dn);
+			addReplica();
+			testMap.put(keyRange, dataNodeList);
+			keyList.add(keyRange);
 			nodes.add(dn);
 		}
+	}
+	
+	private void addReplica(){
+		for(Map.Entry<Long, List<DataNode>> entry : testMap.entrySet()){
+			Long key = entry.getKey();
+			List<DataNode> retrievedList = entry.getValue();
+			//read number of replica nodes from properties or config file
+			int numOfReplicas = 3;
+			for(DataNode tempNode : retrievedList){
+				for(int i=0; i<nodes.size();i++){
+					int j = i;
+					DataNode orgNode = nodes.get(i);
+					if(tempNode.getId().equals(orgNode.getId())){
+						if(i==nodes.size()-1){
+							j=-1;
+						}
+						for(int k=0;k<numOfReplicas;k++){
+							j++;
+							retrievedList.add(nodes.get(j));	
+						}
+					}
+				}
+				
+			}
+			
+		}
+	}
+	
+	public static void main(String args[]){
+		private TreeMap<Long, List<DataNode>> testMap = HashRangeMap.getInstance().getRangeMap();
+		sampleMap.put("1", );
 	}
 }
