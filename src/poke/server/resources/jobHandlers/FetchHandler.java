772980815672368f6dfa871ec1fd0dbb762d86e4
Navya Gattupally <navyagattupally@ymail.com>
2014-11-05 17:21:30 -0800

    failure handler.

--- src/poke/server/resources/jobHandlers/FetchHandler.java
+++ src/poke/server/resources/jobHandlers/FetchHandler.java
@@ -14,52 +14,69 @@ import eye.Comm.Request;
 import eye.Comm.PhotoHeader.ResponseFlag;
 
 public class FetchHandler {
-
-	public Request handleRequest(Request request) {
-		// TODO Auto-generated method stub
-		if(request.getBody().getPhotoPayload().hasUuid())
-		{
-			UUID uuid = UUID.fromString(request.getBody().getPhotoPayload().getUuid());
-			Operations mongoOps= new Operations();
-			
-			
-			//Fetching To MongoDB
-			Map result=mongoOps.fetchPhoto(uuid);
-			String name =(String) result.get("name");
-			byte[] img= (byte[]) result.get("data");
-			ByteString data=ByteString.copyFrom(img);
-			long lastModified=(Long) result.get("lastModified");
-			System.out.println("Name of retrieved photo: " + name);
-			System.out.println("Last Modified on: " + lastModified);
-			Date lastMod=new Date(lastModified);
-
-			Request.Builder rb = Request.newBuilder();
-
-			// Setting Headers
-			rb.setHeader(ResourceUtil.buildPhotoHeader(request,ResponseFlag.success,lastMod));
-
-			// Setting PhotoPayload
-			PhotoPayload.Builder ppb= PhotoPayload.newBuilder();
-			ppb.setUuid(request.getBody().getPhotoPayload().getUuid());
-			//ppb.setData(null);
-			ppb.setName(name);
-			ppb.setData(data);
-
-			
-			//Setting Payload
-			Payload.Builder pb=Payload.newBuilder();
-			pb.setPhotoPayload(ppb.build());
-			rb.setBody(pb.build());
-
-			Request reply = rb.build();
-
-			return reply;
-		}
-		else
-		{
-			FailureHandler fh=new FailureHandler();
-			return fh.handleRequest(request, "Invalid Photo UUID");
-		}
-	}
-
+    
+    public Request handleRequest(Request request) {
+        // TODO Auto-generated method stub
+        if(request.getBody().getPhotoPayload().hasUuid())
+        {
+            UUID uuid=null;
+            try
+            {
+                uuid = UUID.fromString(request.getBody().getPhotoPayload().getUuid());
+            }
+            catch(IllegalArgumentException e)
+            {
+                FailureHandler fh=new FailureHandler();
+                return fh.handleRequest(request, "Invalid Photo UUID Format");
+            }
+            Operations mongoOps= new Operations();
+            
+            
+            //Fetching To MongoDB
+            Map result=mongoOps.fetchPhoto(uuid);
+            if(result.containsKey("Error"))
+            {
+                FailureHandler fh=new FailureHandler();
+                return fh.handleRequest(request, (String)result.get("Error"));
+            }
+            else
+            {
+                String name =(String) result.get("name");
+                byte[] img= (byte[]) result.get("data");
+                ByteString data=ByteString.copyFrom(img);
+                long lastModified=(Long) result.get("lastModified");
+                System.out.println("Name of retrieved photo: " + name);
+                System.out.println("Last Modified on: " + lastModified);
+                Date lastMod=new Date(lastModified);
+                
+                Request.Builder rb = Request.newBuilder();
+                
+                // Setting Headers
+                rb.setHeader(ResourceUtil.buildPhotoHeader(request,ResponseFlag.success,lastMod));
+                
+                // Setting PhotoPayload
+                PhotoPayload.Builder ppb= PhotoPayload.newBuilder();
+                ppb.setUuid(request.getBody().getPhotoPayload().getUuid());
+                //ppb.setData(null);
+                ppb.setName(name);
+                ppb.setData(data);
+                
+                
+                //Setting Payload
+                Payload.Builder pb=Payload.newBuilder();
+                pb.setPhotoPayload(ppb.build());
+                rb.setBody(pb.build());
+                
+                Request reply = rb.build();
+                
+                return reply;
+            }
+        }
+        else
+        {
+            FailureHandler fh=new FailureHandler();
+            return fh.handleRequest(request, "Invalid Photo UUID");
+        }
+    }
+    
 }
--- src/poke/server/storage/mongo/Operations.java
+++ src/poke/server/storage/mongo/Operations.java
@@ -4,6 +4,8 @@ import java.util.Date;
 import java.util.Map;
 import java.util.UUID;
 
+import poke.server.resources.jobHandlers.FailureHandler;
+
 import com.google.protobuf.ByteString;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DBCollection;
@@ -12,46 +14,53 @@ import com.mongodb.DBObject;
 import com.mongodb.util.JSON;
 
 public class Operations {
-	
-	public void savePhoto(UUID uuid, String name, ByteString img, long lastModified)
-	{
-		Connection mongo= new Connection();
-		DBCollection imgStorage = mongo.getConnection();
-		byte[] image=img.toByteArray();
-		BasicDBObject doc = new BasicDBObject("uuid", uuid)
+    
+    public void savePhoto(UUID uuid, String name, ByteString img, long lastModified)
+    {
+        Connection mongo= new Connection();
+        DBCollection imgStorage = mongo.getConnection();
+        byte[] image=img.toByteArray();
+        BasicDBObject doc = new BasicDBObject("uuid", uuid)
         .append("name", name)
         .append("data", image)
         .append("lastModified", lastModified);
-		imgStorage.insert(doc);
-		//imgStorage.save(doc);
-		mongo.closeConnection();
-	}
-	
-	public Map fetchPhoto(UUID uuid)
-	{
-		Connection mongo= new Connection();
-		DBCollection imgStorage = mongo.getConnection();
-		BasicDBObject query = new BasicDBObject("uuid", uuid);
-		DBCursor cursor = imgStorage.find(query);
-		Map result=null;
-		try {
-		   while(cursor.hasNext()) {
-		       result=cursor.next().toMap();
-		   }
-		} finally {
-		   cursor.close();
-		}
-		mongo.closeConnection();
-		return result;
-	}
-	
-	public void deletePhoto(UUID uuid)
-	{
-		Connection mongo= new Connection();
-		DBCollection imgStorage = mongo.getConnection();
-		BasicDBObject query = new BasicDBObject("uuid", uuid);
-		imgStorage.remove(query);
-		mongo.closeConnection();
-	}
-
+        imgStorage.insert(doc);
+        //imgStorage.save(doc);
+        mongo.closeConnection();
+    }
+    
+    public Map fetchPhoto(UUID uuid)
+    {
+        Connection mongo= new Connection();
+        DBCollection imgStorage = mongo.getConnection();
+        BasicDBObject query = new BasicDBObject("uuid", uuid);
+        DBCursor cursor = imgStorage.find(query);
+        
+        Map result=null;
+        try
+        {
+            if(cursor.hasNext())
+            {
+                result=cursor.next().toMap();
+            }
+            else
+            {
+                result.put("Error", "No Image corresponding to the entered UUID");
+            }
+        } finally {
+            cursor.close();
+        }
+        mongo.closeConnection();
+        return result;
+    }
+    
+    public void deletePhoto(UUID uuid)
+    {
+        Connection mongo= new Connection();
+        DBCollection imgStorage = mongo.getConnection();
+        BasicDBObject query = new BasicDBObject("uuid", uuid);
+        imgStorage.remove(query);
+        mongo.closeConnection();
+    }
+    
 }
